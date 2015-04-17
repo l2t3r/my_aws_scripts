880372632cedd61e556b8930a7b11214c31cbfc5
ashley <ashley@hacker.com>
2015-04-17 05:32:26 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,39 @@
+ Simple python script to backup a list of volumes. The script will also delete snapshots after 2 weeks.
+
+ Both volume ids and retension can be configured
+
+ You will require a IAM user with the following permission
+ 
+ {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "Snapshotpolicy",
+            "Effect": "Allow",
+            "Action": [
+                "ec2:CreateSnapshot",
+                "ec2:DescribeInstanceAttribute",
+                "ec2:DescribeInstanceStatus",
+                "ec2:DescribeInstances",
+                "ec2:DescribeSnapshotAttribute",
+                "ec2:DescribeSnapshots",
+                "ec2:DescribeVolumeAttribute",
+                "ec2:DescribeVolumeStatus",
+                "ec2:DescribeVolumes",
+                "ec2:ReportInstanceStatus",
+                "ec2:CreateTags",
+                "ec2:ResetSnapshotAttribute",
+                "ec2:DescribeAvailabilityZones",
+		"ec2:DeleteSnapshot",
+                "ec2:DescribeTags"
+            ],
+            "Resource": [
+                "*"
+            ]
+        }
+    ]
+}
+
+ you are best to use instance role and then remove the parameters in the connection string for ec2
+ 
+
---
+++ ebssnapshot.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# Simple script to create ebs snapshots for volume list and cleanup of snapshots after n of days
+
+import boto
+from boto import ec2
+from datetime import datetime
+from datetime import timedelta
+
+now = datetime.now()
+today = now.date()
+
+#Definitions
+num_of_days_to_keep=13
+volume_list = { 'portaldata': 'vol-42329615','bankdata': 'vol-0c72dc3ei' }
+
+conn =  boto.ec2.connect_to_region("ap-southeast-2", aws_access_key_id='AAAAAAAAAAAAAAAAAAAAAAAAA', aws_secret_access_key='BBBBBBBBBBBBBBBBBBBBBBBBBBBBBB')
+
+for name,id in volume_list.items():
+        print "%s id is %s"% (name,id)
+        desc = "%s-%s" % (name,today)
+        snap_obj = conn.create_snapshot(id, description=desc)
+        if (snap_obj):
+                snap_obj.add_tags({'user': 'portalsnapshot'})
+        print "%s snapshot id %s"% (name,snap_obj.id)
+
+two_weeks_ago = (today - timedelta(days=num_of_days_to_keep))
+
+for name,id in volume_list.items():
+
+        OldDesc =  "%s-%s" % (name,two_weeks_ago)
+
+        snapshots = conn.get_all_snapshots(filters = {"description": OldDesc})
+
+        for i in snapshots:
+                print i.id
+                try:
+                        print "delete snapshot %s" % i.id
+                        conn.delete_snapshot(i.id)
+                except Exception, err:
+                        print Exception, err
